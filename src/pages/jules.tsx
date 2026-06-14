069babe2bee1961408c3324307edf1a149f8a594
notasianrizz <andrew@zandi.vip>
2026-06-14 19:54:04 -0400

    Remove jules static page and associated component from the project

--- src/pages/jules.tsx
+++
@@ -1,6 +0,0 @@
-import { StaticHtmlPage } from '@/components/StaticHtmlPage';
-import { julesDocument } from '@/content/static-pages/jules';
-
-export default function Page() {
-  return <StaticHtmlPage document={julesDocument} />;
-}
